87d59ad8873533f0ba053d0a09539a774ec8a852
jamileth-kumpanam <jamilethkumpanam@MacBook-Air-de-Jamileth.local>
2025-05-25 22:46:13 -0500

    panel

--- src/main/java/ec/edu/ups/solicitudCompra/Principal.java
+++ src/main/java/ec/edu/ups/solicitudCompra/Principal.java
@@ -1,7 +1,7 @@
 package ec.edu.ups.solicitudCompra;
 
 import ec.edu.ups.solicitudCompra.view.MenuPrincipalView;
-
+//Presenta panel
 public class Principal {
     public static void main(String[] args) {
         new MenuPrincipalView(); // Inicia la aplicación
